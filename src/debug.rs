6254c6c7ed4be1f582dc22e9a7e2491a062d792f
albert-abzalimov <alb.abzalimov@gmail.com>
2023-05-26 08:52:01 -0400

    Day 1: starting debug function

---
+++ src/debug.rs
@@ -0,0 +1,13 @@
+// Place for temporary debugging stuff
+use crate::{WIDTH, HEIGHT};
+
+fn draw_board(board: &Board) {
+    // draw lines
+    // King:     Ki
+    // Bishop:   Bi
+    // Pawn:     Pa
+    // Knight:   Kn
+    // Rook:     Ro
+    // Queen:    Qu
+    String output = String::new();
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
+mod debug;
+use debug::*;
+
+const WIDTH: u8 = 8;
+const HEIGHT: u8 = 8;
+
 fn main() {
     println!("Hello, world!");
 }
