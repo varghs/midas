761bd6ee6d25ab70665954abd279c96876ad7787
varghs <svarghs8531@gmail.com>
2023-05-26 13:04:02 -0400

    testing

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,51 @@
+use std::convert::From;
+use std::ops::BitAnd;
+
+enum Piece {
+    Pawn = 2,
+    Rook = 3,
+    Knight = 4,
+    Bishop = 5,
+    Queen = 6,
+    King = 7,
+}
+
+enum Color {
+    White = 0,
+    Black = 1,
+}
+
+#[derive(Clone, Copy)]
 pub struct Bitboard(pub u64);
 
+impl From<u64> for Bitboard {
+    fn from(n: u64) -> Self {
+        Bitboard(n)
+    }
+}
+
+impl BitAnd for Bitboard {
+    type Output = Self;
+
+    fn bitand(self, rhs: Self) -> Self::Output {
+        Self(self.0 & rhs.0)
+    }
+}
+
 pub struct Board {
-    white_pawn: Bitboard,
-    white_rook: Bitboard,
-    white_knight: Bitboard,
-    white_bishop: Bitboard,
-    white_queen: Bitboard,
-    white_king: Bitboard,
-    black_pawn: Bitboard,
-    black_rook: Bitboard,
-    black_knight: Bitboard,
-    black_bishop: Bitboard,
-    black_queen: Bitboard,
-    black_king: Bitboard,
+    boards: [Bitboard; 8],
+}
+
+impl Board {
+    fn get_piece(&self, p: Piece) -> Bitboard {
+        self.boards[p as usize]
+    }
+
+    fn get_color(&self, c: Color) -> Bitboard {
+        self.boards[c as usize]
+    }
+
+    fn get_piece_of_color(&self, p: Piece, c: Color) -> Bitboard {
+        self.get_piece(p) & self.get_color(c)
+    } 
 }
