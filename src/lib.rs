7e29faf0aeb4eb5911301b1941b4015d5d9260cd
varghs <svarghs8531@gmail.com>
2023-05-30 16:09:21 -0400

    enumeration for squares (i don't know why i didn't do this earlier)

--- src/lib.rs
+++ src/lib.rs
@@ -6,16 +6,31 @@ macro_rules! get_bit {
     };
 }
 
+#[rustfmt::skip]
+enum Square {
+  a1, b1, c1, d1, e1, f1, g1, h1,
+  a2, b2, c2, d2, e2, f2, g2, h2,
+  a3, b3, c3, d3, e3, f3, g3, h3,
+  a4, b4, c4, d4, e4, f4, g4, h4,
+  a5, b5, c5, d5, e5, f5, g5, h5,
+  a6, b6, c6, d6, e6, f6, g6, h6,
+  a7, b7, c7, d7, e7, f7, g7, h7,
+  a8, b8, c8, d8, e8, f8, g8, h8
+}
+
 pub type Bitboard = u64;
 
 pub fn print_bitboard(b: Bitboard) {
-    for rank in (0..=7).rev() {
+    for rank in (0..8).rev() {
         for file in 0..8 {
             let square = rank * 8 + file;
-            print!("{} ", match get_bit!(b, square) {
-                0 => 0,
-                _ => 1,
-            });
+            print!(
+                "{} ",
+                match get_bit!(b, square) {
+                    0 => 0,
+                    _ => 1,
+                }
+            );
         }
         print!("\n");
     }
@@ -61,7 +76,6 @@ impl Board {
         self.get_piece(p) & self.get_color(c)
     }
 }
-
 
 // TODO!
 /*
