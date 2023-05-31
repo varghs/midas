548af89e490c00cb3c8462880b8a07352f4ba1ae
varghs <svarghs8531@gmail.com>
2023-05-31 08:52:00 -0400

    figured out default board

--- src/main.rs
+++ src/main.rs
@@ -6,10 +6,15 @@ use debug::*;
 use midas::engine::{bitboard::{print_bitboard, Bitboard}, board::{self, Board}};
 
 fn main() {
-    let pawns: Bitboard = 0x00000000000000FF;
+    let pawns: Bitboard = 0x00FF00000000FF00;
+    let rooks: Bitboard = 0x8100000000000081;
+    let knights: Bitboard = 0x4200000000000042;
+    let bishops: Bitboard = 0x2400000000000024;
+    let queens: Bitboard = 0x0800000000000008;
+    let kings: Bitboard = 0x1000000000000010;
     let def: Bitboard = 0;
 
-    let boards: [Bitboard; 8] = [def, def, pawns, def, def, def, def, def];
+    let boards: [Bitboard; 8] = [def, def, pawns, rooks, knights, bishops, queens, kings];
 
     let board: Board = Board { boards };
 
