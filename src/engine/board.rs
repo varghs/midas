3a9a07eb033024f827a81ed88fc3d6f87ed74e56
varghs <svarghs8531@gmail.com>
2023-06-17 15:29:19 -0400

    restructuring fen

--- src/engine/board.rs
+++ src/engine/board.rs
@@ -2,7 +2,6 @@ use super::{bitboard::{Bitboard, print_bitboard}, attacks::AttackTables};
 use std::{convert::TryFrom, ops::Not};
 use std::fmt::Display;
 use crate::engine::square::Square;
-use super::fen::FEN;
 
 use crate::{get_bit, set_bit};
 
@@ -98,7 +97,7 @@ pub enum Castle {
     BlackQueen = 8,
 }
 
-pub struct CastleRep(u8);
+pub struct CastleRep(pub u8);
 
 impl CastleRep {
     pub fn new() -> Self {
@@ -204,84 +203,6 @@ impl Board {
 
     pub fn get_piece_of_color(&self, p: Piece, c: Color) -> Bitboard {
         self.get_piece(p) & self.get_color(c)
-    }
-
-    pub fn parse_fen(&mut self, fen: FEN) {
-        self.boards.fill(0);
-        self.side = Color::White;
-        self.en_passant_sq = None;
-        self.castle = CastleRep(0);
-
-        let mut fen_iter = fen.0.chars();
-
-        let mut break_condition = false;
-
-        let mut rank: i32 = 7;
-        let mut file: i32 = 0;
-
-        while rank >= 0 {
-            while file < 8 {
-                let square = rank * 8 + file;
-                let char = fen_iter.next().unwrap();
-                if char >= 'a' && char <= 'z' {
-                    let piece: Piece = char.try_into().unwrap_or(Piece::Pawn);
-                    set_bit!(self.boards[piece as usize], square);
-                    set_bit!(self.boards[Color::Black as usize], square);
-                    file += 1;
-                } else if char >= 'A' && char <= 'Z' {
-                    let piece: Piece = char.try_into().unwrap_or(Piece::Pawn);
-                    set_bit!(self.boards[piece as usize], square);
-                    set_bit!(self.boards[Color::White as usize], square);
-                    file += 1;
-                } else if char >= '0' && char <= '9' {
-                    let offset = (char as i32) - ('0' as i32);
-                    file += offset;
-                } else if char == '/' {
-                } else {
-                    break_condition = true;
-                    break;
-                }
-            }
-            rank -= 1;
-            file = 0;
-            if break_condition {
-                break;
-            }
-        }
-        fen_iter.next();
-        match fen_iter.next() {
-            Some('w') => self.side = Color::White,
-            Some('b') => self.side = Color::Black,
-            _ => panic!("Invalid side in FEN.")
-        }
-
-        fen_iter.next();
-        let mut char = fen_iter.next().unwrap();
-        while char != ' ' {
-            match char {
-                'K' => self.castle.set_castle(Castle::WhiteKing),
-                'Q' => self.castle.set_castle(Castle::WhiteQueen),
-                'k' => self.castle.set_castle(Castle::BlackKing),
-                'q' => self.castle.set_castle(Castle::BlackQueen),
-                '-' => {},
-                _ => panic!("Invalid castling in FEN.")
-            }
-            char = fen_iter.next().unwrap();
-        }
-
-        char = fen_iter.next().unwrap();
-        let mut sq = String::new();
-        if char != '-' {
-            sq.push(char);
-            sq.push(fen_iter.next().unwrap());
-            self.en_passant_sq = Some(sq.as_str().try_into().unwrap());
-        } else {
-            self.en_passant_sq = None;
-        }
-
-        println!("{}", fen_iter.as_str());
-
-        // todo: half move clock and fullmove counter
     }
 
     pub fn is_square_attacked(&self, square: Square, side: Color) -> bool {
